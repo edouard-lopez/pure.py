1486b814b72d9e39e506a662065087e4b1ea7730
Edouard Lopez <contact@edouard-lopez.com>
2019-03-18 23:55:40 +0100

    ensure ZSH configuration directory exist in docker

--- install/configure.zsh
+++ install/configure.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+mkdir -p $HOME/.zsh/
 cp $HOME/.pure/config/prompt.zsh $HOME/.zsh/
 echo 'export PURE_EXECUTABLE_PATH=$HOME/.pure/' >> $HOME/.zshrc
 echo 'source $HOME/.zsh/prompt.zsh' >> $HOME/.zshrc

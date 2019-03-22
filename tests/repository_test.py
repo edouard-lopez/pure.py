8796c57c13cd18abed3121fd63cc9443de220c65
Edouard Lopez <contact@edouard-lopez.com>
2019-03-22 01:28:17 +0100

    refactor repository.active_branch() to a segment
        repository.ActiveBranch().segment()

--- tests/repository_test.py
+++ tests/repository_test.py
@@ -3,20 +3,37 @@ import tempfile
 import git
 
 from pure import colors, repository
+from pure.prompt import fetch
 
 
-def test_contains_git_branch_name():
+def test_dummy_repository_has_no_branch_name():
+    with tempfile.TemporaryDirectory() as tmp_repo:
+        assert '' == repository.ActiveBranch(tmp_repo).raw()
+
+
+def test_active_branch_contains_raw_git_branch_name():
     with tempfile.TemporaryDirectory() as tmp_repo:
         empty_repo = git.Repo.init(tmp_repo)
 
-        assert 'master' in str(repository.active_branch(tmp_repo))
+        assert 'master' == repository.ActiveBranch(tmp_repo).raw()
+
+
+def test_active_branch_segment_contains_text_and_style():
+    with tempfile.TemporaryDirectory() as tmp_repo:
+        empty_repo = git.Repo.init(tmp_repo)
+
+        segment = repository.ActiveBranch(tmp_repo).segment()
+
+        assert segment == {'text': 'master', 'style': colors.mute}
 
 
 def test_git_branch_name_color_is_mute():
     with tempfile.TemporaryDirectory() as tmp_repo:
         empty_repo = git.Repo.init(tmp_repo)
 
-        assert str(repository.active_branch(tmp_repo)) == str(colors.mute('master'))
+        assert str(fetch(repository.ActiveBranch(tmp_repo).segment())) == str(colors.mute('master'))
+
+
 
 
 def test_displays_when_repo_is_dirty():

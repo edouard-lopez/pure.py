dc33d73d32bd891b2de288560c4bb37ea68dcfbc
Edouard Lopez <contact@edouard-lopez.com>
2021-01-01 23:55:05 +0100

    refactor pending_push_commit test using a decorator

--- tests/repository_pending_commits.py
+++ tests/repository_pending_commits.py
@@ -12,20 +12,26 @@ def test_dummy_directory_has_no_pending_commits():
         assert repository.PendingCommit(repo).raw() == constants.NOTHING
 
 
-def test_repository_pending_push_commit_contains_raw_symbol():
-    with tempfile.TemporaryDirectory() as remote_directory:
-        remote_repo = git.Repo.init(remote_directory, bare=True)
+def setup_repo(func):                                                                                            
+    def wrapper():                                                                                            
+        with tempfile.TemporaryDirectory() as remote_directory:
+            remote_repo = git.Repo.init(remote_directory, bare=True)
 
-        with tempfile.TemporaryDirectory() as repo_directory:
-            repo = git.Repo.init(repo_directory)
-            origin = repo.create_remote('origin', url=remote_directory)
-            with tempfile.NamedTemporaryFile(dir=repo_directory) as file:
-                repo.index.add([file.name])
-                repo.index.commit("init file")
-            origin.push()
+            with tempfile.TemporaryDirectory() as repo_directory:
+                repo = git.Repo.init(repo_directory)
+                func(repo, remote_repo)
+    return wrapper 
 
-            with tempfile.NamedTemporaryFile(dir=repo_directory) as file_missing_upstream:
-                repo.index.add([file_missing_upstream.name])
-                repo.index.commit("file yet to be pushed")
+@setup_repo
+def test_repository_pending_PUSH_commit_contains_raw_symbol(repo, remote_repo):
+    origin = repo.create_remote('origin', url=remote_repo.working_dir)
+    with tempfile.NamedTemporaryFile(dir=repo.working_dir) as file:
+        repo.index.add([file.name])
+        repo.index.commit("init file")
+    origin.push()
 
-            assert repository.PendingCommit(repo_directory).raw() == '⇡'
+    with tempfile.NamedTemporaryFile(dir=repo.working_dir) as file_missing_upstream:
+        repo.index.add([file_missing_upstream.name])
+        repo.index.commit("file yet to be pushed")
+
+    assert repository.PendingCommit(repo.working_dir).raw() == '⇡'
